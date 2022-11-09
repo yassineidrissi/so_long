128a3f6558b0370302bd855fb12a5f0f192da89a
Yassine Idrissi <yaidriss@c2r1p6.1337.ma>
2022-11-09 20:18:11 +0100

    valid test caracters

--- validation.c
+++ validation.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   validation.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: yaidriss <yaidriss@student1337.com>        +#+  +:+       +#+        */
+/*   By: yaidriss <yaidriss@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/30 19:06:10 by yaidriss          #+#    #+#             */
-/*   Updated: 2022/11/09 19:38:20 by yaidriss         ###   ########.fr       */
+/*   Updated: 2022/11/09 20:17:38 by yaidriss         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -57,11 +57,44 @@ void test_walls(t_map *map)
 	while(++i < map->hight)
 		if(map->map[i][0] != '1' || map->map[i][map->width - 1] != '1')
 			handl_errors(3);
-	printf("%d\n", i);
+}
+
+void test_caraters(t_map *map)
+{
+	int i;
+	int j;
+
+	i = -1;
+	map->count_p = 0;
+	map->count_e = 0;
+	map->count_c = 0;
+	while(++i < map->hight)
+	{
+		j = -1;
+		while(map->map[i][++j])
+		{
+			if (map->map[i][j] == 'P')
+				map->count_p++;	
+			else if (map->map[i][j] == 'C')
+				map->count_c++;
+			else if (map->map[i][j] == 'E')
+				map->count_e++;
+		}
+	}
+	// printf("%d\n %d %d", map->count_c, map->count_e, map->count_p);
+	if(map->count_p != 1 || map->count_e != 1 || map->count_c < 1)
+		handl_errors(9);
+}
+
+void test_valid_map(t_map *map)
+{
+	
 }
 
 void	validation(t_map	*map)
 {
 	test_rectangular(map);
 	test_walls(map);
+	test_caraters(map);
+	test_valid_map(map)
 }
